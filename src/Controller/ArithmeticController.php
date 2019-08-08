57f518575ec013d5bd05d755b56bac2e03ce6f8d
cyberde <megatau@gmail.com>
2019-08-08 23:30:40 +0300

    fix

--- src/Controller/ArithmeticController.php
+++ src/Controller/ArithmeticController.php
@@ -1,1 +1,20 @@
 <?php
+
+namespace cyberde\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArithmeticController
+{
+  /**
+   * @Route("/arithmetic/math/{method}/{expression}", name="cyberde_arithmetic")
+   */
+
+  public function math($method, $expression)
+  {
+    return new Response(
+      '<html><body>method: '.$method.' expression: '.$expression.'</body></html>'
+    );
+  }
+}
